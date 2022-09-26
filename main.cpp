8b4bf0f9cdfdf500b9e4bc0af95785533e46fa17
Nykyta <nykyta.kudrya@gmail.com>
2022-09-26 16:22:45 +0200

    Last commit before push

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
-
-
+#include "Testy1/sayings.hpp"
 
 int main()
 {
-    std::cout << "Hello World" << std::endl;
+    Sayings say;
+
+    std::cout << "Everything below is printed from a submodule:" << std::endl;
+    say.say_hello();
+    say.say_team();
+    say.say_update();
+    say.say_goodbye();
 }
