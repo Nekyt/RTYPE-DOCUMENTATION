3135ff57cc0e2996477f6e8d03030e9b8d6f4628
Kevin <kevin.zilliox@epitech.eu>
2022-10-10 15:56:02 +0200

    test multiple client on server, server close co when all four connect

--- client/main.cpp
+++ client/main.cpp
@@ -5,25 +5,53 @@
 ** main
 */
 
-#include "./GameClient.hpp"
+//#include "./GameClient.hpp"
+#include <SFML/Network.hpp>
+#include <iostream>
 
-int program()
+//int program()
+//{
+//    GameClient client;
+//
+//    client.gameLoop();
+//    return 0;
+//}
+
+int main(int ac, char **av)
 {
-    GameClient client;
+    sf::UdpSocket socket;
+    sf::IpAddress recipient = "127.0.0.1";
+    unsigned short port = atoi(av[1]);
+    sf::Packet packet;
+    int x = -1;
+    int co = 0;
 
-    client.gameLoop();
-    return 0;
-}
-
-int main()
-{
-    try {
-        return program();
-    } catch(std::exception& e) {
-        std::cerr << "Error (stdexcept): " << e.what() << '\n';
-        return 84;
-    } catch (...) {
-        std::cerr << "Error: Unknown exception !!!!!!\n";
-        return 84;
+    if (socket.bind(sf::Socket::AnyPort) != sf::Socket::Done) {
+        printf("fail\n");
+        return (84);
     }
+    printf("lisening on port %d\n", socket.getLocalPort());
+    packet << x;
+        if (socket.send(packet, recipient, port) != sf::Socket::Done) {
+            printf("fail send\n");
+            exit (84);
+        }
+    while (co == 0) {
+         if (socket.receive(packet, recipient, port) != sf::Socket::Done) {
+            printf("fail receive\n");
+            exit (84);
+        }
+        std::cout << "Received from " << recipient << " on port " << port << std::endl;
+        packet >> co;
+    }
+    std::cout << "test sucess" << std::endl;
+    //try {
+    //    return program();
+    //} catch(std::exception& e) {
+    //    std::cerr << "Error (stdexcept): " << e.what() << '\n';
+    //    return 84;
+    //} catch (...) {
+    //    std::cerr << "Error: Unknown exception !!!!!!\n";
+    //    return 84;
+    //}
 }
--- server/main.cpp
+++ server/main.cpp
@@ -11,11 +11,36 @@
 int main()
 {
     sf::UdpSocket socket;
+    sf::IpAddress sender;
+    unsigned short port;
+    sf::Packet packet;
+    int id = 0;
+    int co = 1;
+    std::vector<std::pair<sf::IpAddress, unsigned short>> clients_co;
 
     if (socket.bind(sf::Socket::AnyPort) != sf::Socket::Done) {
         printf("fail\n");
         return (84);
     }
     printf("lisening on port %d\n", socket.getLocalPort());
+    while (id != 4) {
+        if (socket.receive(packet, sender, port) != sf::Socket::Done) {
+            printf("fail receive\n");
+            exit (84);
+        }
+        std::cout << "Received from " << sender << " on port " << port << std::endl;
+        clients_co.push_back(std::make_pair(sender, port));
+        id++;
+        std::cout << "connextion register" << std::endl;
+    }
+    for(int i = 0; i < clients_co.size(); i++) {
+        std::cout << "inform client on " << clients_co[i].first << " " << clients_co[i].second << " stop the connection" << std::endl;
+        packet << co << id;
+        id--;
+        if (socket.send(packet, clients_co[i].first, clients_co[i].second) != sf::Socket::Done) {
+            printf("fail send\n");
+            exit (84);
+        }
+    }
     return (0);
 }
