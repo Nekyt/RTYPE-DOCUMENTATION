b6fcd3d69e011f9fb0d6e0184918589ee25d0375
Kevin <kevin.zilliox@epitech.eu>
2022-10-10 13:23:13 +0200

    open any port for upd

--- server/main.cpp
+++ server/main.cpp
@@ -6,9 +6,16 @@
 */
 
 #include <iostream>
+#include <SFML/Network.hpp>
 
 int main()
 {
-    std::cout << "This is the server main." << std::endl;
-    return 0;
+    sf::UdpSocket socket;
+
+    if (socket.bind(sf::Socket::AnyPort) != sf::Socket::Done) {
+        printf("fail\n");
+        return (84);
+    }
+    printf("lisening on port %d\n", socket.getLocalPort());
+    return (0);
 }
