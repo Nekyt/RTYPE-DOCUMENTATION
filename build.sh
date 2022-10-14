0f5117b05a485ef24113608c9f67d97ff4ff385e
Romanie De Meyer <lilith@MacBook-Pro-de-Romanie.local>
2022-10-14 15:15:20 +0200

    change build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
-cd build/
+conan remote add bincrafters https://bincrafters.jfrog.io/artifactory/api/conan/public-conan
+conan remote add conancenter https://center.conan.io
+conan config set general.revisions_enabled=1
+mkdir build
+cd build
+conan install .. --build missing -s cppstd=gnu20
 cmake ..
-cd ..
 make
