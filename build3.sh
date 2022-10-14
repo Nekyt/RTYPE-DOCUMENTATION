41e9650628403c82587be4a118503a60f046576f
SylvianBurn <sylvian.burn@epitech.eu>
2022-10-14 16:48:16 +0200

    Adding all the cmakeliststhat i found but they don't work on my manjaro

---
+++ build3.sh
@@ -0,0 +1,5 @@
+mkdir -p build; cd build && conan install .. --build=missing  -s compiler.libcxx=libstdc++11 && cmake -DCMAKE_BUILD_TYPE=Debug .. && cmake --build  . -- -j 3
+
+cp ./r-type_client ..
+
+cp ./r-type_server ..
